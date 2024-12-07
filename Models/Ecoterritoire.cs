704e7463af9ad870e7afdad444ea53ed70a7dc40
Nathaniel Kwok <natkwok902@gmail.com>
2024-12-06 21:19:14 -0500

    Created Model and Scaffolded Context

---
+++ Models/Ecoterritoire.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+
+namespace asp_mvc_webmap_vs.Models;
+
+public partial class Ecoterritoire
+{
+    public int Id { get; set; }
+
+    public Polygon? Geom { get; set; }
+
+    public string? Text { get; set; }
+
+    public double? ShapeLeng { get; set; }
+
+    public double? ShapeArea { get; set; }
+}
